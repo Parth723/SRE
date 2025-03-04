09db5d0f502a220fc3b3ba7e274d26998ecfb7eb
Parth723 <parthbelwate007@gmail.com>
2025-03-04 19:43:31 +0530

    spec file for controller

--- src/students/students.controller.spec.ts
+++ src/students/students.controller.spec.ts
@@ -2,19 +2,47 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 
+const mockStudentsService = {
+  findAll: jest.fn().mockResolvedValue([{ id: 1, name: 'John Doe' }]),
+  findOne: jest.fn().mockResolvedValue({ id: 1, name: 'John Doe' }),
+  create: jest.fn().mockResolvedValue({ id: 1, name: 'John Doe' }),
+  remove: jest.fn().mockResolvedValue({ affected: 1 })
+};
+
 describe('StudentsController', () => {
   let controller: StudentsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentsController],
-      providers: [StudentsService],
+      providers: [{ provide: StudentsService, useValue: mockStudentsService }],
     }).compile();
 
     controller = module.get<StudentsController>(StudentsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return all students', async () => {
+    expect(await controller.findAll()).toEqual([{ id: 1, name: 'John Doe' }]);
+  });
+
+  it('should return a student by ID', async () => {
+    expect(await controller.findOne('1')).toEqual({ id: 1, name: 'John Doe' });
+  });
+
+  it('should create a student', async () => {
+    expect(
+      await controller.create({
+        firstName: 'John',
+        lastName: 'Doe',
+        class: '10th',
+        division: 'A',
+        age: 15,
+      })
+    ).toEqual({ id: 1, name: 'John Doe' });
+  });
+  
+
+  it('should delete a student', async () => {
+    expect(await controller.remove('1')).toEqual({ affected: 1 });
   });
 });
